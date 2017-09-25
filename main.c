85f26a482293991d57d80b8ae6d3d5b5d9b98dcc
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-09-25 14:43:23 -0400

    testing stdin

--- main.c
+++ main.c
@@ -7,28 +7,33 @@
 void printTable (char *** table, int lastRowIndex);
 
 int main(int argc, const char * argv[]) {
+    
+    char fun[20];
+    fgets(fun, 20, stdin);
+    
+    printf("%s\n", fun);
 
-    char *** table = (char ***) malloc(4194304 * sizeof(char **));
-    char line[4096];
-    int row = 0;
-    while(!feof(stdin)) {
-        
-        fgets(line, 4096, stdin);
-        table[row] = tokenizeRow(line);
-        row += 1;
-    }
-    
-    if(realloc(table, (row + 1) * sizeof(char **))) {
-        return 1;
-    }
-    
-    printf("ass nigga\n");
-    
-    printf("%s\n", table[0][0]);
-    
-    printTable(table, row);
-    
-    return 0;
+//    char *** table = (char ***) malloc(4194304 * sizeof(char **));
+//    char line[4096];
+//    int row = 0;
+//    while(!feof(stdin)) {
+//
+//        fgets(line, 4096, stdin);
+//        table[row] = tokenizeRow(line);
+//        row += 1;
+//    }
+//
+//    if(realloc(table, (row + 1) * sizeof(char **))) {
+//        return 1;
+//    }
+//
+//    printf("ass nigga\n");
+//
+//    printf("%s\n", table[0][0]);
+//
+//    printTable(table, row);
+//
+//    return 0;
 }
 
 void printTable (char *** table, int lastRowIndex) {
