4426751f885d50ecd37a1afc055876575963ded3
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-09-26 06:59:05 -0400

    ...

--- main.c
+++ main.c
@@ -8,32 +8,31 @@ void printTable (char *** table, int lastRowIndex);
 
 int main(int argc, const char * argv[]) {
     
-    char fun[4096];
-    while(fgets(fun, 4096, stdin) != NULL) {
-        printf("%s\n", fun);
+//    char fun[4096];
+//    while(fgets(fun, 4096, stdin) != NULL) {
+//        printf("%s\n", fun);
+//    }
+
+    char *** table = (char ***) malloc(4194304 * sizeof(char **));
+    char line[4096];
+    int row = 0;
+    while(fgets(line, 4096, stdin) != NULL) {
+
+        table[row] = tokenizeRow(line);
+        row += 1;
     }
 
-//    char *** table = (char ***) malloc(4194304 * sizeof(char **));
-//    char line[4096];
-//    int row = 0;
-//    while(!feof(stdin)) {
-//
-//        fgets(line, 4096, stdin);
-//        table[row] = tokenizeRow(line);
-//        row += 1;
-//    }
-//
 //    if(realloc(table, (row + 1) * sizeof(char **))) {
 //        return 1;
 //    }
-//
-//    printf("ass nigga\n");
-//
-//    printf("%s\n", table[0][0]);
-//
-//    printTable(table, row);
-//
-//    return 0;
+
+    printf("ass nigga\n");
+
+    printf("%s\n", table[0][0]);
+
+    printTable(table, row);
+
+    return 0;
 }
 
 void printTable (char *** table, int lastRowIndex) {
